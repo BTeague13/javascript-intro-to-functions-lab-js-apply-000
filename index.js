7a2849e097234e95120a721db7dcfbb6983825c1
Brandon Teague <brandon.teague13@icloud.com>
2019-09-26 10:21:08 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,24 @@
+function shout(string) {
+  
+  return string.toUpperCase()
+}
+function whisper(string){
+  return string.toLowerCase()
+}
+
+
+function logShout(string) {
+const spy = "hello";
+console.log(spy.toUpperCase());
+}
+
+
+function logWhisper(string) {
+  const spy = "hello";
+  console.log(spy.toLowerCase());
+}
+
+function sayHiToGrandma (string){
+  
+  return string.
+}
